dd11279a77523ba494e5721470b4010722e3af79
luis996 <52387429+luis996@users.noreply.github.com>
2022-05-07 14:09:01 -0500

    Add Bedrock axe and the texture

--- src/main/java/net/mcreator/onemod/item/BedrockAxeItem.java
+++ src/main/java/net/mcreator/onemod/item/BedrockAxeItem.java
@@ -6,7 +6,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.ShovelItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
@@ -16,16 +16,16 @@ import net.mcreator.onemod.itemgroup.OneModItemGroup;
 import net.mcreator.onemod.OnemodModElements;
 
 @OnemodModElements.ModElement.Tag
-public class BedrockSwordItem extends OnemodModElements.ModElement {
-	@ObjectHolder("onemod:bedrock_sword")
+public class BedrockAxeItem extends OnemodModElements.ModElement {
+	@ObjectHolder("onemod:bedrock_axe")
 	public static final Item block = null;
-	public BedrockSwordItem(OnemodModElements instance) {
-		super(instance, 1);
+	public BedrockAxeItem(OnemodModElements instance) {
+		super(instance, 5);
 	}
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new SwordItem(new IItemTier() {
+		elements.items.add(() -> new ShovelItem(new IItemTier() {
 			public int getMaxUses() {
 				return 128000;
 			}
@@ -47,14 +47,14 @@ public class BedrockSwordItem extends OnemodModElements.ModElement {
 			}
 
 			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack(Blocks.BEDROCK), new ItemStack(Blocks.DIAMOND_BLOCK));
+				return Ingredient.fromStacks(new ItemStack(Blocks.DIAMOND_BLOCK), new ItemStack(Blocks.BEDROCK));
 			}
-		}, 3, 96f, new Item.Properties().group(OneModItemGroup.tab).isImmuneToFire()) {
+		}, 1, 96f, new Item.Properties().group(OneModItemGroup.tab).isImmuneToFire()) {
 			@Override
 			@OnlyIn(Dist.CLIENT)
 			public boolean hasEffect(ItemStack itemstack) {
 				return true;
 			}
-		}.setRegistryName("bedrock_sword"));
+		}.setRegistryName("bedrock_axe"));
 	}
 }
