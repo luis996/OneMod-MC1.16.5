c0ff57268d5c7d1233e0bd97a64dee47c8e958d1
luis996 <52387429+luis996@users.noreply.github.com>
2022-05-23 16:08:21 -0500

    Added bedrock shovel, craft, translantion

---
+++ src/main/java/net/mcreator/onemod/item/BedrockShovelItem.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.onemod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.onemod.itemgroup.OneModItemGroup;
+import net.mcreator.onemod.OnemodModElements;
+
+@OnemodModElements.ModElement.Tag
+public class BedrockShovelItem extends OnemodModElements.ModElement {
+	@ObjectHolder("onemod:bedrock_shovel")
+	public static final Item block = null;
+	public BedrockShovelItem(OnemodModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ShovelItem(new IItemTier() {
+			public int getMaxUses() {
+				return 128000;
+			}
+
+			public float getEfficiency() {
+				return 128000f;
+			}
+
+			public float getAttackDamage() {
+				return 127998f;
+			}
+
+			public int getHarvestLevel() {
+				return 128000;
+			}
+
+			public int getEnchantability() {
+				return 128000;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(Blocks.BEDROCK), new ItemStack(Blocks.DIAMOND_BLOCK));
+			}
+		}, 1, 96f, new Item.Properties().group(OneModItemGroup.tab).isImmuneToFire()) {
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("bedrock_shovel"));
+	}
+}
